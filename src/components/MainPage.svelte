cbb4f656267b0977da194531e6a9021f17dad301
longph <longphamduy2002@gmail.com>
2023-01-13 03:20:15 +0200

    main page (empty for now)

---
+++ src/components/MainPage.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<div id="skills">
+    
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. 
+Mauris varius diam vitae arcu. Sed arcu lectus auctor vitae, consectetuer et venenatis eget velit. 
+Sed augue orci, lacinia eu tincidunt et eleifend nec lacus. Donec ultricies nisl ut felis, suspendisse potenti. 
+Lorem ipsum ligula ut hendrerit mollis, ipsum erat vehicula risus, eu suscipit sem libero nec erat. 
+Aliquam erat volutpat. Sed congue augue vitae neque. Nulla consectetuer porttitor pede. 
+Fusce purus morbi tortor magna condimentum vel, placerat id blandit sit amet tortor.
+
+
+</div>
+
+<div id="about">Lorem ipsulm dolor sit amet rem adipisci delectus? consectetur adipisicing elit. 
+    Ipsam cum, voluptate vero, blanditiis voluptatum aut consectetur et in quam rem adipisci delectus enim quod a laudantium perferendis ad possimus culpa?
+</div>
+
+<style>
+    div {
+        margin-left: 16%;
+    }
+</style>
