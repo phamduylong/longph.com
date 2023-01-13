857abcb8265e70d4444d27eaff08a5426af32531
longph <longphamduy2002@gmail.com>
2023-01-14 00:03:07 +0200

    remove unused file

--- src/components/ProgressBar.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { tweened } from 'svelte/motion';
-    const progress = tweened(0.75);
-</script>
-<div><p>C++</p><progress value={$progress} ></progress></div>
-
-<style>
-</style>
