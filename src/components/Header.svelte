e9a7b7529580209f2b05d585249bcec192affc4b
longph <longphamduy2002@gmail.com>
2023-01-13 03:21:14 +0200

    progress bar

---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<header>
+    <strong>Long Pham</strong>
+</header>
+
+<style>
+    header {
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        color: white;
+        font-size: 4vw;
+        height: 20%;
+        width: 100%;
+        background-color: coral;
+        padding-left: 4vw;
+        margin-left: 15%;
+    }
+</style>
---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { tweened } from 'svelte/motion';
+    const progress = tweened(0.75);
+</script>
+<div><p>C++</p><progress value={$progress} ></progress></div>
+
+<style>
+</style>
