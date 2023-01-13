a94100e003df3a5944c781a157e93576d5e9f14e
longph <longphamduy2002@gmail.com>
2023-01-14 01:36:42 +0200

    code reorganize, increase scrollbar shadow & add icon border

--- src/components/SideTab.svelte
+++ src/components/SideTab.svelte
@@ -2,57 +2,52 @@
 	const img_url = new URL('../assets/profile_pic.jpg', import.meta.url).href;
 	import { Progressbar } from 'flowbite-svelte';
 	const skills = [
-		{skill: "NodeJs", progress: "70"},
-		{skill: "MongoDB", progress: "50"},
-		{skill: "SQL", progress: "50"},	
-		{skill: "Svelte", progress: "50"}, 
-		{skill: "HTML5", progress: "50"},
-		{skill: "CSS3", progress: "40"},
-		{skill: "C", progress: "60"},
-		{skill: "C++", progress: "70"},
-		{skill: "Embedded Programming", progress: "50"},
-		{skill: "Python", progress: "60"},
-		{skill: "Robot Framework", progress: "50"},
-		{skill: "Jenkins", progress: "40"},
-		{skill: "Linux", progress: "50"},
-		{skill: "Git", progress: "70"},
-		];
+		{ skill: 'NodeJs', progress: '70' },
+		{ skill: 'Express', progress: '70' },
+		{ skill: 'EJS Engine', progress: '60' },
+		{ skill: 'MongoDB', progress: '50' },
+		{ skill: 'SQL', progress: '50' },
+		{ skill: 'Svelte', progress: '50' },
+		{ skill: 'HTML5', progress: '50' },
+		{ skill: 'CSS3', progress: '40' },
+		{ skill: 'Embedded Programming', progress: '50' },
+		{ skill: 'C', progress: '60' },
+		{ skill: 'C++', progress: '70' },
+		{ skill: 'Python', progress: '60' },
+		{ skill: 'Robot Framework', progress: '50' },
+		{ skill: 'Jenkins', progress: '40' },
+		{ skill: 'Linux', progress: '50' },
+		{ skill: 'Git', progress: '70' }
+	];
 </script>
 
 <div class="sidenav">
 	<div id="icon_container"><img id="icon" src={img_url} alt="avatar" /></div>
-	
+
 	<strong id="skills_section_header">Skills</strong>
 	<div id="skills_container">
 		{#each skills as skill, index}
-			<div class="skill-items">{skill.skill}<Progressbar progress={skill.progress} size="h-3" color="blue" /></div>
+			<div class="skill-items">
+				{skill.skill}<Progressbar progress={skill.progress} size="h-3" color="blue" />
+			</div>
 		{/each}
 	</div>
 </div>
 
 <style>
-	.skill-items {
-		position: relative;
-		display: inline-block;
-		height: 2%;
-		margin: 0 auto;
-		padding: 10%;
-		font-size: 1vw;
-		width: 100%;
-	}
 
 	::-webkit-scrollbar {
 		width: 12px;
 	}
 
 	::-webkit-scrollbar-track {
-		-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+		-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
 		border-radius: 10px;
 	}
 
 	::-webkit-scrollbar-thumb {
 		border-radius: 10px;
-		-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+		-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.7);
 	}
 
 	.sidenav {
@@ -78,6 +73,17 @@
 		top: 5%;
 	}
 
+
+	#icon {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		border: 2px solid black;
+		border-radius: 50%;
+		width: 50%;
+	}
+
 	#skills_section_header {
 		position: absolute;
 		top: 35%;
@@ -101,12 +107,14 @@
 		width: 100%;
 	}
 
-	#icon {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		border-radius: 50%;
-		width: 50%;
+	.skill-items {
+		position: relative;
+		display: inline-block;
+		height: 2%;
+		margin: 0 auto;
+		padding: 10%;
+		font-size: 1vw;
+		width: 100%;
 	}
+
 </style>
