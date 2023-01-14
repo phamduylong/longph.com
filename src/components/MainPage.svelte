d6cb447d3d956cd46da78b02b9b812b0bc45d7f2
longph <longphamduy2002@gmail.com>
2023-01-14 22:33:15 +0200

    add main page content (align issue still)

--- src/components/MainPage.svelte
+++ src/components/MainPage.svelte
@@ -1,27 +1,36 @@
 <script>
-
 </script>
 
-<div id="skills">
-    
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. 
-Mauris varius diam vitae arcu. Sed arcu lectus auctor vitae, consectetuer et venenatis eget velit. 
-Sed augue orci, lacinia eu tincidunt et eleifend nec lacus. Donec ultricies nisl ut felis, suspendisse potenti. 
-Lorem ipsum ligula ut hendrerit mollis, ipsum erat vehicula risus, eu suscipit sem libero nec erat. 
-Aliquam erat volutpat. Sed congue augue vitae neque. Nulla consectetuer porttitor pede. 
-Fusce purus morbi tortor magna condimentum vel, placerat id blandit sit amet tortor.
-
-
-</div>
-
-<div id="about">Lorem ipsulm dolor sit amet rem adipisci delectus? consectetur adipisicing elit. 
-    Ipsam cum, voluptate vero, blanditiis voluptatum aut consectetur et in quam rem adipisci delectus enim quod a laudantium perferendis ad possimus culpa?
+<div id="main">
+	<div id="name">Long Pham</div>
+	<div id="hr" />
+	<div id="title">Junior Developer</div>
 </div>
 
 <style>
-    div {
-        margin-left: 25%;
-        margin-right:5%
-    }
+
+	#name {
+		position: absolute;
+		top: 5%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		font-size: 4vw;
+	}
+
+	#hr {
+		position: absolute;
+		top: 15%;
+		left: 50%;
+		width: 50%;
+		transform: translate(-25%, -50%);
+		border-top: black 5px solid;
+	}
+
+	#title {
+		position: absolute;
+		top: 20%;
+		left: 50%;
+		transform: translate(-25%, -50%);
+		font-size: 2vw;
+	}
 </style>
