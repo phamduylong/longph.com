c44e575b2d770a9820fdefadd43800f7034ab127
longph <longphamduy2002@gmail.com>
2023-01-15 02:53:09 +0200

    new components for main page

---
+++ src/components/Section.svelte
@@ -0,0 +1,19 @@
+<script>
+	import SectionItem from "./SectionItem.svelte";
+
+    export let title = "";
+</script>
+
+<div id="title">
+    {title}
+    <SectionItem title="METROPOLIA UNIVERSITY OF APPLIED SCIENCES" timeline="2020-2024 (Estimated)" content="Major in Embedded Systems Programming
+    "/>
+</div>
+
+<style>
+    #title {
+        color: #00802b;
+        font-size: 3vw;
+        margin: 10%;
+    }
+</style>
---
+++ src/components/SectionItem.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let title = '',
+		timeline = '2023', content = '';
+</script>
+
+<div id="item">
+	<div id="title">
+		<strong>{title}</strong>
+	</div>
+
+	<div id="timeline">
+        <strong>{timeline}</strong>
+    </div>
+
+    <div id="content">
+        {content}
+    </div>
+</div>
+
+<style>
+    #item {
+        font-size: 1vw;
+        color: black;
+    }
+
+    #title {
+        font-size: 1.5vw;
+    }
+</style>
