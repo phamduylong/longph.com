ccb441f8be73d97ea56efbcaff053dc32a835a1f
longph <longphamduy2002@gmail.com>
2023-01-15 14:31:23 +0200

    icon and skill item component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,24 @@
+<script>
+	const img_url = new URL('../assets/pp.jpg', import.meta.url).href;
+</script>
+
+<div id="icon_container"><img id="icon" src={img_url} alt="avatar" /></div>
+
+<style>
+	#icon_container {
+		height: 20%;
+		width: 100%;
+		position: absolute;
+		top: 5%;
+	}
+
+	#icon {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		border: 2px solid black;
+		border-radius: 50%;
+		width: 50%;
+	}
+</style>
---
+++ src/components/SkillItem.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let skill = '',
+		progress = '';
+	import { Progressbar } from 'flowbite-svelte';
+</script>
+
+<div class="skill-items">
+	{skill}<Progressbar {progress} size="h-3" color="green" />
+</div>
+
+<style>
+	.skill-items {
+		position: relative;
+		display: inline-block;
+		height: 2%;
+		margin: 0 auto;
+		padding: 5%;
+		font-size: 1.25vw;
+		width: 100%;
+	}
+</style>
