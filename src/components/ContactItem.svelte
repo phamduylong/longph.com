8ae9e707ecc78217c8615f0d9fc2bcb56aaf37af
longph <longphamduy2002@gmail.com>
2023-01-15 17:05:54 +0200

    add contactitem component

---
+++ src/components/ContactItem.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let img = "", content = "duy-long-pham", url= "https://linkedin.com/in/duy-long-pham";
+</script>
+<div id="wrapper">
+    <a href={url} target="blank"><img align="center" src= {img} alt="duy-long-pham"/>
+        {content}
+    </a>
+</div>
+
+
+<style>
+    #wrapper {
+        margin: 5%;
+        width: 100%;
+        font-size: 0.9vw;
+    }
+
+    img {
+        height: 8%;
+        width: 8%;
+        display: inline-block;
+    }
+
+    a {
+        display: inline-block;
+    }
+</style>
