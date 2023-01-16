c22c2cbedebfd5aa8ec79eeea446ccc9918bb87b
longph <longphamduy2002@gmail.com>
2023-01-16 21:33:14 +0200

    change data scheme

--- src/assets/data.js
+++ src/assets/data.js
@@ -8,6 +8,21 @@ const github_img_url = new URL('../assets/github.svg', import.meta.url).href;
 const phone_img_url = new URL('../assets/phone.svg', import.meta.url).href;
 const whatsapp_img_url = new URL('../assets/whatsapp.svg', import.meta.url).href;
 
+const arduino_img_url = new URL('../assets/tech/arduino.svg', import.meta.url).href;
+const c_img_url = new URL('../assets/tech/c.svg', import.meta.url).href;
+const chartjs_img_url = new URL('../assets/tech/chartjs.svg', import.meta.url).href;
+const cpp_img_url = new URL('../assets/tech/cpp.svg', import.meta.url).href;
+const css3_img_url = new URL('../assets/tech/css3.svg', import.meta.url).href;
+const ejs_img_url = new URL('../assets/tech/ejs.svg', import.meta.url).href;
+const express_img_url = new URL('../assets/tech/express.svg', import.meta.url).href;
+const html5_img_url = new URL('../assets/tech/html5.svg', import.meta.url).href;
+const javascript_img_url = new URL('../assets/tech/javascript.svg', import.meta.url).href;
+const mongodb_img_url = new URL('../assets/tech/mongodb.svg', import.meta.url).href;
+const nodejs_img_url = new URL('../assets/tech/nodejs.svg', import.meta.url).href;
+const python_img_url = new URL('../assets/tech/python.svg', import.meta.url).href;
+const robotframework_img_url = new URL('../assets/tech/robotframework.svg', import.meta.url).href;
+const svelte_img_url = new URL('../assets/tech/svelte.svg', import.meta.url).href;
+const sveltekit_img_url = new URL('../assets/tech/sveltekit.svg', import.meta.url).href;
 
 const section_datas = [
 
@@ -50,10 +65,15 @@ const section_datas = [
                 timeline: "2023", 
                 content: `My first project with Svelte! All of us have had that "To Do List" project when we first started with front-end development right? I personally don't use 
                 ToDo lists so I thought a Shopping List would be a bit more beneficial. This project was for me to learn the basics front-end concepts: props, modules, libraries, 
-                deploying and much more than that.<br/><br/>
-                Tech Stack: Svelte, SvelteKit, HTML, CSS<br/><br/>
+                deploying and much more than that.<br/><br/>`,
+                stack: [svelte_img_url, html5_img_url, css3_img_url, javascript_img_url],
+                links: [
+                    {href: "https://shopping-list-phamduylong.vercel.app/", text: "Demo App"},
+                    {href: "https://github.com/phamduylong/ShoppingList", text: "GitHub repository"}
+                ]
+                /*Tech Stack: Svelte, SvelteKit, HTML, CSS<br/><br/>
                 <a href="https://shopping-list-e3c6g2ttu-phamduylong.vercel.app/" style="color: blue;">Demo App</a><br/>
-                <a href="https://github.com/phamduylong/ShoppingList" style="color: blue;">GitHub repository</a>`
+                <a href="https://github.com/phamduylong/ShoppingList" style="color: blue;">GitHub repository</a>`*/
             },
 
             {
@@ -65,9 +85,14 @@ const section_datas = [
                 specifically CO2 level, relative humidity and temperature, then using the collected information as a feedback for the ventilation fan 
                 to adjust power level so as to create the ideal state in terms of air conditions. I contributed to building the back-end server for the project,
                 where we takes data from the physical devices through MQTT, visualize it with Chart.js and creating a User Interface so as to interact with the devices remotely. 
-                One other task that I took on this project was to write automation test cases with Robot Framework and Selenium.<br/><br/>
+                One other task that I took on this project was to write automation test cases with Robot Framework and Selenium.<br/><br/>`,
+                stack: [c_img_url, cpp_img_url, nodejs_img_url, express_img_url, ejs_img_url, css3_img_url, mongodb_img_url, chartjs_img_url, robotframework_img_url],
+                links: [
+                    {href: "https://github.com/phamduylong/abb-ventilation-controller", text: "GitHub repository"}
+                ]
+                /*
                 Tech Stack: C, C++, NodeJs, Express, EJS, CSS, MongoDB, Chart.js<br/><br/>
-                <a href="https://github.com/phamduylong/abb-ventilation-controller" style="color: blue;">GitHub repository</a>`
+                <a href="https://github.com/phamduylong/abb-ventilation-controller" style="color: blue;">GitHub repository</a>`*/
             },
 
             {
@@ -75,19 +100,26 @@ const section_datas = [
                 timeline: "2022", 
                 content: `An IoT Device to play legendary ring tones. This project is about digital audio, whereas an RTTTL string will be sent by the website 
                 using voice recognition to the Raspberry Pico through MQTT, or chosen from the LCD interface and the string will be parsed into playable audio 
-                and played through the piezo-buzzer. My role in this project was to mainly work with the Raspberry Pi Pico W using MicroPython.<br/><br/>
-                Tech Stack: Python, NodeJs, Express, EJS, CSS, MongoDB<br/><br/>
+                and played through the piezo-buzzer. My role in this project was to mainly work with the Raspberry Pi Pico W using MicroPython.<br/><br/>`,
+                stack: [python_img_url, nodejs_img_url, express_img_url, ejs_img_url, css3_img_url, mongodb_img_url, chartjs_img_url],
+                links: [
+                    {href: "https://github.com/IoT-project-groups-1-2/g2-voice-controller", text: "GitHub repository"}
+                ]
+                /*
                 <a href="https://www.youtube.com/watch?v=tQpu81TbtpA" style="color: blue;">Demo Video</a><br/>
-                 <a href="https://github.com/IoT-project-groups-1-2/g2-voice-controller" style="color: blue;">GitHub repository</a>`
+                 <a href="https://github.com/IoT-project-groups-1-2/g2-voice-controller" style="color: blue;">GitHub repository</a>`*/
             },
 
             {
                 title: "LCD Timer",
                 timeline: "2022",
                 content: `A hobby project made in the summer break 🙂 It is a timer device with an LCD display. User chooses an amount of time, 
-                the system buzzes after the timer is up with a piezo buzzer.<br/><br/>
-                Tech Stack: C++<br/><br/>
-                <a href="https://github.com/phamduylong/LCD-Timer" style="color: blue;">GitHub repository</a>`
+                the system buzzes after the timer is up with a piezo buzzer.<br/><br/>`,
+                stack: [cpp_img_url, arduino_img_url],
+                links: [
+                    {href: "https://github.com/phamduylong/LCD-Timer", text: "GitHub repository"}
+                ]
+                /*<a href="https://github.com/phamduylong/LCD-Timer" style="color: blue;">GitHub repository</a>`*/
             },
 
             {
@@ -95,10 +127,15 @@ const section_datas = [
                 timeline: "2020", 
                 content: `A Discord bot to fetch quotes and jokes through an API and send it to Discord server chatboxes. Quotes are fetch from 
                 <a href="https://zenquotes.io" style="color: blue;">here</a> and jokes are acquired from PyJokes. The chatbot was previously hosted on Heroku 
-                but has gone offline frequently when Heroku dropped their free plans 😥<br/><br/>
-                Tech Stack: Python, discord.py<br/><br/>
+                but has gone offline frequently when Heroku dropped their free plans 😥<br/><br/>`,
+                stack: [python_img_url],
+                links: [
+                    {href: "https://discord.com/oauth2/authorize?client_id=823841679257763841&permissions=2048&scope=bot", text: "Invite Bot"},
+                    {href: "https://github.com/phamduylong/CuteBot", text: "GitHub repository"}
+                ]
+                /*
                 <a href="https://discord.com/oauth2/authorize?client_id=823841679257763841&permissions=2048&scope=bot" style="color: blue;">Invite bot</a><br/>
-                <a href="https://github.com/phamduylong/CuteBot" style="color: blue;">GitHub repository</a>`
+                <a href="https://github.com/phamduylong/CuteBot" style="color: blue;">GitHub repository</a>`*/
             },
 
             {
@@ -106,10 +143,15 @@ const section_datas = [
                 timeline: "2020",
                 content: `The application was based on Dev Ed's Rock Paper Scissors Game tutorial as a part of JavaScript course at Metropolia UAS (Helsinki, Finland). 
                 The application is pretty straight-forward, and easy to use. Click Let's Play to begin playing, and Play Again to reset results to 0-0. 
-                On each play, click on the option you want to choose (either Rock, Paper or Scissors), and wait for the result.<br/><br/>
-                Tech Stack: HTML, CSS, JavaScript<br/><br/>
+                On each play, click on the option you want to choose (either Rock, Paper or Scissors), and wait for the result.<br/><br/>`,
+                stack: [html5_img_url, css3_img_url, javascript_img_url],
+                links: [
+                    {href: "https://phamduylong.github.io/rockpaperscissors/", text: "Demo App"},
+                    {href: "https://github.com/phamduylong/rockpaperscissors", text: "GitHub repository"}
+                ]
+                /*
                 <a href="https://phamduylong.github.io/rockpaperscissors/" style="color: blue;">Demo App</a><br/>
-                <a href="https://github.com/phamduylong/rockpaperscissors" style="color: blue;">GitHub repository</a>`
+                <a href="https://github.com/phamduylong/rockpaperscissors" style="color: blue;">GitHub repository</a>`*/
             }
 
         ]
