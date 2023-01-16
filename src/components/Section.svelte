7fbefecb57d782e0f09b4f8f12d14c56ae202a1d
longph <longphamduy2002@gmail.com>
2023-01-16 21:34:55 +0200

    changed plain text into logos and style hyperlinks

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -7,7 +7,25 @@
 <div id="title">
     {section_data.title.toUpperCase()}
     {#each section_data.entries as item, index}
-        <SectionItem title={item.title} timeline={item.timeline} content={item.content}/>
+        <SectionItem title={item.title} timeline={item.timeline} content={item.content}>
+            {#if item.stack !== undefined}
+            <div class="stack-container">
+                Tech Stack:
+                {#each item.stack as tech}
+                    <img src={tech} alt={tech} class="tech-item"/>
+                {/each}
+            </div>
+                
+            {/if}
+            
+            {#if item.links !== undefined}
+            <br/><br/>
+                {#each item.links as hl}
+                    <a href="{hl.href}" target="blank">{hl.text}</a><br/>
+                {/each}
+            {/if}
+
+        </SectionItem>
     {/each}
 </div>
 
@@ -16,5 +34,20 @@
         color: #338566;
         font-size: 3vw;
         margin: 10%;
+    }
+
+    a {
+        text-decoration: underline;
+    }
+
+    a:hover {
+        color: #338566;
+    }
+
+    .tech-item {
+        display: inline-block;
+        height: 5%;
+        width: 5%;
+        margin-left: 1%;
     }
 </style>
