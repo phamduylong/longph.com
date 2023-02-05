25cc1f1e28add512b6e136d58530e6d103b6270d
longph <longphamduy2002@gmail.com>
2023-02-05 23:26:21 +0200

    set transparent background, resolves #6

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 {#await theme.init()}
 <div></div>
 {:then}
-	<ThemeSwitch classes="absolute top-1 right-2 text-base md:top-2 md:right-6 md:text-lg lg:top-6 lg:right-10 lg:text-3xl" />
+	<ThemeSwitch classes="bg-transparent absolute top-1 right-2 text-base md:top-2 md:right-6 md:text-lg lg:top-6 lg:right-10 lg:text-3xl" />
 
 	<slot />
 {/await}
