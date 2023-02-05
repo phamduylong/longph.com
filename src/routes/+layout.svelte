5bfc2489903f885f55641c5f6988a2df156d6762
longph <longphamduy2002@gmail.com>
2023-02-05 04:04:57 +0200

    add theme switch to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.postcss';
+	import ThemeSwitch from '../lib/ThemeSwitch.svelte';
 </script>
+
+<ThemeSwitch classes="absolute top-1 right-2 text-base md:top-2 md:right-6 md:text-lg lg:top-6 lg:right-10 lg:text-3xl" />
 
 <slot />
