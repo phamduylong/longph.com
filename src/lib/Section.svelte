2b5b6442a42fa288def6be58a41b2b7d419020e7
longph <longphamduy2002@gmail.com>
2023-02-19 15:51:22 +0200

    make iframe lazy loading and allow yt fullscreen

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -1,6 +1,6 @@
 <script>
 	import SectionItem from './SectionItem.svelte';
-    import TechIcon from './TechIcon.svelte';
+	import TechIcon from './TechIcon.svelte';
 	export let section_data;
 
 	function removeWhitespaces(str) {
@@ -9,14 +9,21 @@
 </script>
 
 <div id="wrapper">
-	<p id={removeWhitespaces(section_data.title)}><a class="text-primary-green no-underline" href={"#" + removeWhitespaces(section_data.title)}>{section_data.title.toUpperCase()}</a></p>
+	<p id={removeWhitespaces(section_data.title)}>
+		<a class="text-primary-green no-underline" 
+		href={'#' + removeWhitespaces(section_data.title)}
+			>{section_data.title.toUpperCase()}</a
+		>
+	</p>
 	{#each section_data.entries as item}
-		<SectionItem title={item.title} timeline={item.timeline} content={item.content}>
+		<SectionItem title={item.title} 
+		timeline={item.timeline} 
+		content={item.content}>
 			{#if item.stack !== undefined && item.stack !== []}
 				<div class="stack-container">
 					<b>Tech Stack:</b><br />
 					{#each item.stack as tech}
-                        <TechIcon parentSection={item.title} {tech}/>
+						<TechIcon parentSection={item.title} {tech} />
 					{/each}
 				</div>
 			{/if}
@@ -25,8 +32,18 @@
 				<br /><br />
 				{#each item.media as media}
 					{#if media.type === 'youtube'}
-						<a href={"https://www.youtube.com/watch?v=" + media.ytid} target="_blank" rel="noreferrer" class="underline hover:text-primary-green">{media.caption}
-							<iframe src={"https://www.youtube.com/embed/" + media.ytid} title="Demo"></iframe>
+						<a
+							href={'https://www.youtube.com/watch?v=' + media.ytid}
+							target="_blank"
+							rel="noreferrer"
+							class="underline hover:text-primary-green"
+							>{media.caption}
+							<iframe
+								src={'https://www.youtube.com/embed/' + media.ytid}
+								title="Demo"
+								loading="lazy"
+								allowfullscreen={true}
+							/>
 						</a>
 						<br />
 					{/if}
@@ -36,18 +53,30 @@
 			{#if item.links !== undefined && item.links !== []}
 				<br /><br />
 				{#each item.links as hl}
-					{#if hl.type === "demo"}
-						<a href={hl.href} target="_blank" rel="noreferrer" class="underline hover:text-primary-green">{hl.text}<iframe src={hl.href} title="Demo"></iframe></a><br />
+					{#if hl.type === 'demo'}
+						<a
+							href={hl.href}
+							target="_blank"
+							rel="noreferrer"
+							class="underline hover:text-primary-green"
+							>{hl.text}
+							<iframe src={hl.href} title="Demo" loading="lazy" />
+						</a><br />
 					{:else}
-						<a href={hl.href} target="_blank" rel="noreferrer" class="underline hover:text-primary-green">{hl.text}</a><br />
+						<a
+							href={hl.href}
+							target="_blank"
+							rel="noreferrer"
+							class="underline hover:text-primary-green">
+							{hl.text}
+						</a
+						><br />
 					{/if}
 				{/each}
 			{/if}
 		</SectionItem>
 	{/each}
 </div>
-
-
 
 <style>
 	#wrapper {
