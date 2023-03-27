d8c5ee26d53a4ed04ce6c7437bb52fce87950f43
longph <longphamduy2002@gmail.com>
2023-03-27 23:05:27 +0300

    remove popover info

--- src/lib/TechIcon.svelte
+++ src/lib/TechIcon.svelte
@@ -1,19 +1,6 @@
 <script lang="ts">
 	import type { TechIcon } from './assets/types';
 	export let tech : TechIcon = { img: '', alt: '', url: ''};
-	import { slide } from 'svelte/transition';
-	import DeviceDetector from 'svelte-device-detector';
-	import { Popover } from 'svelte-smooth-popover';
-	import spinner from '$lib/assets/loading_gif.gif';
-	function openPopover() : void {
-		showPopover = true;
-	}
-	function closePopover() : void {
-		showPopover = false;
-	}
-	let hoverIframe : NodeJS.Timeout;
-	let loaded : boolean = false;
-	let showPopover : boolean = false;
 </script>
 
 <a href={tech.url} target="_blank" rel="noreferrer">
@@ -21,45 +8,7 @@
 		src={tech.img}
 		alt={tech.alt}
 		class="tech-item"
-		on:mouseover={openPopover}
-		on:focus
-		on:mouseleave={() => {
-			hoverIframe = setTimeout(() => {
-				closePopover();
-			}, 100);
-		}}
 	/>
-	<DeviceDetector showInDevice="desktop">
-		<Popover
-			transition={slide}
-			align={'top-right'}
-			open={showPopover}
-			caretWidth={3}
-			caretBg="#E5E4E2"
-			offset={50}
-		>
-			<div class="rounded-full bg-primary-green shadow-sm">
-				{#if !loaded}
-					<div class="flex h-full w-full items-center justify-center">
-						<img id="spinner" src={spinner} alt="Loading" />
-					</div>
-				{/if}
-				<iframe
-					class="h-full w-full rounded-full p-1"
-					src={tech.url}
-					title={tech.url}
-					on:load={() => {
-						loaded = true;
-					}}
-					on:mouseover={() => {
-						clearTimeout(hoverIframe);
-					}}
-					on:focus
-					on:mouseleave={closePopover}
-				/>
-			</div>
-		</Popover>
-	</DeviceDetector>
 </a>
 
 <style>
@@ -80,14 +29,5 @@
 
 	a {
 		font-size: 0;
-	}
-
-	#spinner {
-		height: 4vh;
-		width: 4vh;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
 	}
 </style>
