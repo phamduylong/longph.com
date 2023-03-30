c757f070103650dc27c9a961d18091ff4d33c956
longph <longphamduy2002@gmail.com>
2023-03-30 08:49:43 +0300

    add new absolute center base class and custom values for padding and margin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 const config = {
+	important: true,
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
@@ -7,6 +10,9 @@ const config = {
 	theme: {
 		extend: {
 			margin: {
+				'1/100': '1%',
+				'1/50': '2%',
+				'1/20': '5%',
 				'1/10': '10%',
 				'1/5': '20%',
 				'1/4': '25%',
@@ -17,7 +23,16 @@ const config = {
 			},
 
 			padding: {
-				'1/100': '1%'
+				'1/100': '1%',
+				'1/50': '2%',
+				'1/20': '5%',
+				'1/10': '10%',
+				'1/5': '20%',
+				'1/4': '25%',
+				'1/3': '33%',
+				'1/2': '50%',
+				'2/3': '67%',
+				'3/4': '75%'
 			},
 
 			fontSize: {
@@ -25,16 +40,33 @@ const config = {
 				'2vw': '2vw',
 				'3vw': '3vw',
 				'4vw': '4vw',
-				'5vw': '5vw'
+				'5vw': '5vw',
+				'6vw': '6vw',
+				'7vw': '7vw',
+				'8vw': '8vw',
+				'9vw': '9vw',
+				'10vw': '10vw'
 			}
 		},
 		colors: {
-			coral: '#ff7f50',
+			'coral': '#ff7f50',
 			'primary-green': '#338566'
 		}
 	},
 
-	plugins: [require('flowbite/plugin')],
+	plugins: [
+		require('flowbite/plugin'), 		
+		plugin(function ({ addBase }) {
+		addBase({
+			'.absolute-center': {
+				position: 'absolute',
+				top: '50%',
+				left: '50%',
+				transform: 'translate(-50%, -50%)'
+			}
+		})
+	}),
+],
 	darkMode: 'class'
 };
 
