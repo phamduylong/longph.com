2a2affcd07313c12e7bf2968972e728341b73084
longph <longphamduy2002@gmail.com>
2023-03-31 09:31:20 +0300

    move from vanilla css to tailwind

--- src/lib/MainPage.svelte
+++ src/lib/MainPage.svelte
@@ -4,60 +4,16 @@
 	import ContactMeForm from './ContactMeForm.svelte';
 </script>
 
-<div class="ml-1/4 mr-1/20 overflow-scroll overflow-x-hidden">
-	<div id="intro">
+<div class="ml-1/4 mr-1/20 overflow-scroll overflow-x-hidden w-full">
+	<div class="absolute top-1/4 left-1/2 w-1/2 -translate-x-[20%] -translate-y-1/2 border-t-[5px] border-solid h-1/5 lg:-translate-x-[30%]">
 		<div class="absolute -top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-4vw">Long Pham</div>
 		<div class="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2vw">Junior Developer</div>
 	</div>
 
-	<div id="sections">
+	<div class="absolute top-1/2 left-[65%] -translate-x-[55%] -translate-y-1/2 h-[60%] w-[60%] max-w-full lg:w-[80%]">
 		{#each section_datas as section_data}
 			<Section {section_data} />
 		{/each}
 		<ContactMeForm />
 	</div>
 </div>
-
-<style>
-
-	#intro {
-		position: absolute;
-		top: 25%;
-		left: 50%;
-		width: 50%;
-		transform: translate(-30%, -50%);
-		border-top: 5px solid;
-		height: 20%;
-	}
-
-	#sections {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-38%, -50%);
-		height: 60%;
-		width: 80%;
-	}
-
-	@media screen and (orientation: portrait) {
-		#intro {
-			position: absolute;
-			top: 25%;
-			left: 50%;
-			width: 50%;
-			transform: translate(-15%, -50%);
-			border-top: 5px solid;
-			height: 20%;
-		}
-
-		#sections {
-			position: absolute;
-			top: 50%;
-			left: 60%;
-			transform: translate(-38%, -50%);
-			height: 60%;
-			max-width: 100%;
-			width: 60%;
-		}
-	}
-</style>
--- src/lib/SideTab.svelte
+++ src/lib/SideTab.svelte
@@ -4,46 +4,10 @@
 	import ContactTab from './ContactTab.svelte';
 </script>
 
-<div class="sidenav">
+<div class="border-x-solid h-full w-[30%] fixed z-[1] top-0 left-0 bg-coral mx-[0.25%] lg:w-1/5">
 	<Icon />
 
 	<SkillTab />
 
 	<ContactTab />
 </div>
-
-<style>
-	.sidenav {
-		border-left: 0.05vw solid;
-		border-right: 0.05vw solid;
-		height: 100%;
-		width: 20%;
-		position: fixed;
-		z-index: 1;
-		top: 0;
-		left: 0;
-		background-color: #fff;
-		overflow-x: hidden;
-		background-color: coral;
-		margin-left: 0.25%;
-		margin-right: 0.25%;
-	}
-
-	@media screen and (orientation: portrait) {
-		.sidenav {
-			border-left: 0.05vw solid;
-			border-right: 0.05vw solid;
-			height: 100%;
-			width: 30%;
-			position: fixed;
-			z-index: 1;
-			top: 0;
-			left: 0;
-			background-color: #fff;
-			overflow-x: hidden;
-			background-color: coral;
-			margin-left: 0.25%;
-			margin-right: 0.25%;
-		}
-	}
-</style>
