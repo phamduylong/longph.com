c724c03b6882a515f4dc0001fd00a6414989dab0
longph <longphamduy2002@gmail.com>
2023-04-01 23:46:39 +0300

    delete old files

--- src/lib/Icon.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import img_url from '$lib/assets/pp.jpg';
-</script>
-
-<div id="icon_container" class="h-1/5 w-full absolute top-[5%]">
-	<img src={img_url} alt="avatar" class="absolute-center border-2 border-solid border-black rounded-[50%] w-1/2">
-</div>
--- src/lib/TechIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { TechIcon } from './assets/types';
-	export let tech: TechIcon = { img: '', alt: '', url: '', text: '' };
-</script>
-
-<a href={tech.url} target="_blank" rel="noreferrer" title={tech.text} class="text-[0]">
-	<img src={tech.img} alt={tech.alt} class="inline-block h-[5%] w-[5%] mx-1/100 mt-[2%] hover:mx-1/50 hover:scale-150" />
-</a>
